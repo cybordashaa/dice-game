82a83fd7d5e526c681d5bf4d62aefad4a018ccbe
dashaa <cybordashaa@gmail.com>
2020-01-25 16:56:38 +0800

    style bolon app.js deer dom bichsen

---
+++ app.js
@@ -0,0 +1,21 @@
+//   Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, 2 дугаар тоглогчийг 1 гэж нэрлэе
+var activePlayer = 0;
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+//  Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+//window.document.querySelector("#score-0").textContent = dice;
+// document.querySelector("#score-1").textContent = dice;
+// document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
+//console.log("Шоо :" + dice);
+
+// Програм эхлэхэд бэлтгэе
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector(".dice").style.display = "none";
+document.querySelector("#current-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
