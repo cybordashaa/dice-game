e8b5e01f951fbb58155a68badf2dcab5601ab4e8
dashaa <cybordashaa@gmail.com>
2020-01-25 18:59:01 +0800

    shoog shiddeg bolgoson

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ var scores = [0, 0];
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
 //window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").textContent = dice;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
 //console.log("Шоо :" + dice);
 
 // Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-document.querySelector("#current-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+var diceDome = document.querySelector(".dice");
+diceDome.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDome.style.display = "block";
+  diceDome.src = "dice-" + diceNumber + ".png";
+});
