64b07b97cc5ba1acc21a1cdc44f68273711049ee
dashaa <cybordashaa@gmail.com>
2020-02-29 14:03:27 +0800

    Togloomiin onoog 100 bolgoloo

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
       scores[activePlayer];
 
     // Уг тоглогч хожсон эсэхийг (оноо нь 100-с их эсэх) шалгах
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       // Тоглоомыг дууссан төлөвт оруулна
       isNewGame = false;
 
